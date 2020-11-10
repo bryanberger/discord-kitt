0baa5447307b8be66fdb1899888bc0a1e920fe01
Bryan Berger <bryan@bryanberger.com>
2020-11-09 22:21:13 -0500

    deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t deca/siribot .
+docker build -t deca/siribot ./bot
 docker tag deca/siribot smartdockerrepo.azurecr.io/deca/siribot
 docker push smartdockerrepo.azurecr.io/deca/siribot
