5f518fe2304d4322aa62d145e67a962ba0050054
Bryan Berger <bryan@bryanberger.com>
2020-11-12 11:42:55 -0500

    fix: voice channel announcing fixes.

--- bot/src/handlers/voiceStateUpdate.ts
+++ bot/src/handlers/voiceStateUpdate.ts
@@ -1,9 +1,13 @@
-import { VoiceState } from 'discord.js'
+import {
+  Collection,
+  VoiceChannel,
+  VoiceConnection,
+  VoiceState,
+} from 'discord.js'
 import { CommandoClient } from 'discord.js-commando'
 
 import { say } from '../lib/announce'
 import { getPhraseForMember } from '../lib/database'
-import guildDelete from './guildDelete'
 
 export default async (
   oldState: VoiceState,
@@ -25,55 +29,69 @@ export default async (
     return
   }
 
-  const oldMember = oldState.member
-  // const channel = newState.channel ?? oldState.channel
   const connections = client.voice.connections
   const connection = connections.get(oldState.guild.id)
-  const username = oldMember.nickname || oldMember.user.username
+  const username = oldState.member.nickname || oldState.member.user.username
+  // const channel = newState.channel ?? oldState.channel
 
-  // const isBotInVoiceChannel = connections.some(
-  //   (connection) => connection.channel === channel,
-  // )
-
-  // if (isBotInVoiceChannel) {
   let message: string = null
 
-  if (!oldState.serverMute && newState.serverMute) {
-    message = 'has been server muted'
-  } else if (oldState.serverMute && !newState.serverMute) {
-    message = 'has been server unmuted'
-  } else if (!oldState.serverDeaf && newState.serverDeaf) {
-    message = 'has been server deafened'
-  } else if (oldState.serverDeaf && !newState.serverDeaf) {
-    message = 'has been server undeafened'
-  } else if (!oldState.deaf && newState.deaf === true) {
-    message = 'has deafened'
-  } else if (oldState.deaf && !newState.deaf) {
-    message = 'has undeafened'
-  } else if (!oldState.mute && newState.mute) {
-    message = 'has muted'
-  } else if (oldState.mute && !newState.mute) {
-    message = 'has unmuted'
-  } else if (newState.channel && oldState.channel?.id !== newState.channel.id) {
+  // Events to be spoken in the bot's channe
+  if (oldState.channelID === newState.channelID) {
+    if (!oldState.serverMute && newState.serverMute) {
+      message = 'has been server muted'
+    } else if (oldState.serverMute && !newState.serverMute) {
+      message = 'has been server unmuted'
+    } else if (!oldState.serverDeaf && newState.serverDeaf) {
+      message = 'has been server deafened'
+    } else if (oldState.serverDeaf && !newState.serverDeaf) {
+      message = 'has been server undeafened'
+    } else if (!oldState.deaf && newState.deaf === true) {
+      message = 'has deafened'
+    } else if (oldState.deaf && !newState.deaf) {
+      message = 'has undeafened'
+    } else if (!oldState.mute && newState.mute) {
+      message = 'has muted'
+    } else if (oldState.mute && !newState.mute) {
+      message = 'has unmuted'
+    }
+
+    if (message !== null) {
+      return await say(connection, `${username} ${message}`)
+    }
+  }
+
+  if (newState.channel && isBotInChannel(connections, newState.channel)) {
+    console.log('join')
     // Check see if this user has a custom `join` message for this guild
     const customMessage = await getPhraseForMember({
-      guildId: oldState.guild.id,
-      memberId: oldMember.id,
+      guildId: newState.guild.id,
+      memberId: newState.member.id,
       type: 'join',
     })
     message = customMessage ?? 'has joined the channel'
-  } else if (oldState.channel && !newState.channel) {
+  } else if (
+    oldState.channel &&
+    isBotInChannel(connections, oldState.channel)
+  ) {
+    console.log('leave')
     // Check see if this user has a custom `leave` message for this guild
     const customMessage = await getPhraseForMember({
       guildId: oldState.guild.id,
-      memberId: oldMember.id,
+      memberId: oldState.member.id,
       type: 'leave',
     })
     message = customMessage ?? 'has left the channel'
   }
 
   if (message !== null) {
-    say(connection, `${username} ${message}`)
+    return await say(connection, `${username} ${message}`)
   }
-  // }
+}
+
+const isBotInChannel = (
+  connections: Collection<string, VoiceConnection>,
+  channel: VoiceChannel,
+) => {
+  return connections.some((connection) => connection.channel.id === channel.id)
 }
