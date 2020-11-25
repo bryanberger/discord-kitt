98a22b6910fb38d8de08c1b6a3bd0237d9c6518b
Bryan Berger <bryan@bryanberger.com>
2020-11-24 21:40:38 -0500

    kludge: every 2 mins play a new Silence() in every connected VC. hopefully this keeps the client from having to be rejoined after a long stretch of inactivity.

--- bot/src/handlers/ready.ts
+++ bot/src/handlers/ready.ts
@@ -1,11 +1,12 @@
-import { VoiceChannel } from "discord.js"
-import { CommandoClient } from "discord.js-commando"
+import { VoiceChannel } from 'discord.js'
+import { CommandoClient } from 'discord.js-commando'
+import { play } from '../lib/announce'
 
-import { channels } from "../lib/database"
-import { setActivity } from "../lib/utils"
+import { channels } from '../lib/database'
+import { Silence } from '../lib/silence'
+import { setActivity } from '../lib/utils'
 
 export default async (client: CommandoClient) => {
-
   // Log
   console.log(
     `Client ready; logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`,
@@ -15,22 +16,27 @@ export default async (client: CommandoClient) => {
   setActivity(client)
 
   // Rejoin all cached channels
-  client.guilds.cache.map(async guild => {
+  client.guilds.cache.map(async (guild) => {
     guild.channels.cache.map(async (channel) => {
       const joinThisChannel = await channels.get(channel.id)
-      if(joinThisChannel) {
+      if (joinThisChannel) {
         const vc = client.channels.cache.get(channel.id) as VoiceChannel
         await vc.join()
       }
     })
   })
 
-  // Hack for now, reconnect to all open voice connections every 10 minutes
-  // setInterval(() => {
-  //   client.voice.connections.map(async (conn) => {
-  //     // conn.channel.leave()
-  //     // await sleep(1000)
-  //     conn.channel.join()
-  //   })
-  // }, 10 * 60 * 1000)
+  // Hack for now, say a Silence in each open voice connection every 2 minutes
+  setInterval(() => {
+    if (client.voice.connections.size > 0) {
+      console.log(
+        `play Silence() in ${client.voice.connections.size} voice channel(s)`,
+      )
+
+      client.voice.connections.map(async (connection) => {
+        await play(connection, new Silence(), { type: 'opus' })
+        connection.setSpeaking(0)
+      })
+    }
+  }, 2 * 60 * 1000)
 }
