e0d940b77092e6d508116d8ba678d7e11574b2ee
Bryan Berger <bryan@bryanberger.com>
2020-12-01 15:56:28 -0500

    ensure dir path

--- bot/src/lib/directories.ts
+++ bot/src/lib/directories.ts
@@ -3,8 +3,8 @@ import path from 'path'
 
 export const ensureDirectories = () => {
   try {
-    fs.ensureDirSync(path.join(__dirname, '../settings'))
-    fs.ensureDirSync(path.join(__dirname, '../cache'))
+    fs.ensureDirSync(path.join(__dirname, '../../settings'))
+    console.log(path.join(__dirname, '../../settings'))
     console.log('Directories ensured!')
   } catch (err) {
     console.error(err)
